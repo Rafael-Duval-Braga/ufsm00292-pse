f37e8a420c525b78abc59de190dbfe91c3a820da
Rafael Braga <rafael.braga@ecomp.ufsm.br>
2024-10-29 17:06:47 -0300

    Update and rename main.c to trabalho_6_pse.c

--- rtos/as_sam_d21/src/trabalho_6_pse.c
+++ rtos/as_sam_d21/src/trabalho_6_pse.c
@@ -41,6 +41,11 @@ void tarefa_6(void);
 void tarefa_7(void);
 void tarefa_8(void);
 
+void tarefa_9(void);
+void tarefa_10(void);
+void tarefa_11(void);
+void tarefa_12(void);
+
 /*
  * Configuracao dos tamanhos das pilhas
  */
@@ -52,6 +57,12 @@ void tarefa_8(void);
 #define TAM_PILHA_6			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_7			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_8			(TAM_MINIMO_PILHA + 24)
+
+#define TAM_PILHA_9         (TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_10        (TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_11        (TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_12        (TAM_MINIMO_PILHA + 24)
+
 #define TAM_PILHA_OCIOSA	(TAM_MINIMO_PILHA + 24)
 
 /*
@@ -65,6 +76,12 @@ uint32_t PILHA_TAREFA_5[TAM_PILHA_5];
 uint32_t PILHA_TAREFA_6[TAM_PILHA_6];
 uint32_t PILHA_TAREFA_7[TAM_PILHA_7];
 uint32_t PILHA_TAREFA_8[TAM_PILHA_8];
+
+uint32_t PILHA_TAREFA_9[TAM_PILHA_9];
+uint32_t PILHA_TAREFA_10[TAM_PILHA_10];
+uint32_t PILHA_TAREFA_11[TAM_PILHA_10];
+uint32_t PILHA_TAREFA_12[TAM_PILHA_10];
+
 uint32_t PILHA_TAREFA_OCIOSA[TAM_PILHA_OCIOSA];
 
 /*
@@ -77,9 +94,17 @@ int main(void)
 	/* Criacao das tarefas */
 	/* Parametros: ponteiro, nome, ponteiro da pilha, tamanho da pilha, prioridade da tarefa */
 	
-	CriaTarefa(tarefa_1, "Tarefa 1", PILHA_TAREFA_1, TAM_PILHA_1, 2);
+//	CriaTarefa(tarefa_1, "Tarefa 1", PILHA_TAREFA_1, TAM_PILHA_1, 2);
 	
-	CriaTarefa(tarefa_2, "Tarefa 2", PILHA_TAREFA_2, TAM_PILHA_2, 1);
+//	CriaTarefa(tarefa_2, "Tarefa 2", PILHA_TAREFA_2, TAM_PILHA_2, 1);
+    
+//    CriaTarefa(tarefa_9, "Tarefa 9", PILHA_TAREFA_9, TAM_PILHA_9, 2);
+    
+//    CriaTarefa(tarefa_10, "Tarefa 10", PILHA_TAREFA_10, TAM_PILHA_10, 1);
+    
+    CriaTarefa(tarefa_11, "Tarefa 11", PILHA_TAREFA_11, TAM_PILHA_11, 1);
+    
+    CriaTarefa(tarefa_12, "Tarefa 12", PILHA_TAREFA_12, TAM_PILHA_12, 2);
 	
 	/* Cria tarefa ociosa do sistema */
 	CriaTarefa(tarefa_ociosa,"Tarefa ociosa", PILHA_TAREFA_OCIOSA, TAM_PILHA_OCIOSA, 0);
@@ -155,12 +180,12 @@ semaforo_t SemaforoTeste = {0,0}; /* declaracao e inicializacao de um semaforo *
 void tarefa_5(void)
 {
 
-	uint32_t a = 0;			/* inicializações para a tarefa */
+	uint32_t a = 0;			/* inicializaï¿½ï¿½es para a tarefa */
 	
 	for(;;)
 	{
 		
-		a++;				/* código exemplo da tarefa */
+		a++;				/* cï¿½digo exemplo da tarefa */
 
 		TarefaEspera(3); 	/* tarefa se coloca em espera por 3 marcas de tempo (ticks) */
 		
@@ -173,19 +198,19 @@ void tarefa_5(void)
 void tarefa_6(void)
 {
 	
-	uint32_t b = 0;	    /* inicializações para a tarefa */
+	uint32_t b = 0;	    /* inicializaï¿½ï¿½es para a tarefa */
 	
 	for(;;)
 	{
 		
-		b++; 			/* código exemplo da tarefa */
+		b++; 			/* cï¿½digo exemplo da tarefa */
 		
 		SemaforoAguarda(&SemaforoTeste); /* tarefa se coloca em espera por semaforo */
 
 	}
 }
 
-/* soluçao com buffer compartihado */
+/* soluï¿½ao com buffer compartihado */
 /* Tarefas de exemplo que usam funcoes de semaforo */
 
 #define TAM_BUFFER 10
@@ -197,7 +222,7 @@ semaforo_t SemaforoVazio = {TAM_BUFFER,0}; /* declaracao e inicializacao de um s
 void tarefa_7(void)
 {
 
-	uint8_t a = 1;			/* inicializações para a tarefa */
+	uint8_t a = 1;			/* inicializaï¿½ï¿½es para a tarefa */
 	uint8_t i = 0;
 	
 	for(;;)
@@ -243,3 +268,68 @@ void tarefa_8(void)
 		SemaforoLibera(&SemaforoVazio);
 	}
 }
+
+
+//------------------------------------
+
+// tarefas 9 e 10 em modo cooperativo
+void tarefa_9(void)
+{
+    static int led = false;
+    
+    for(;;)
+    {
+        port_pin_set_output_level(LED_0_PIN, led);
+        led ^= true;
+        TarefaSuspende(1);
+    }
+}
+
+void tarefa_10(void)
+{
+    
+    static int a = 0;
+    for(;;){
+        TarefaContinua(1);
+        TarefaEspera(1000);
+    }
+}
+
+//------------------------------------
+
+// tarefas 11 e 12 em modo preemptivo
+void tarefa_11(void)
+{
+    static int led = false;
+    
+    for(;;)
+    {
+        port_pin_set_output_level(LED_0_PIN, led);
+        for(volatile int i = 0; i < 1000000; i++);
+        led ^= true;
+    }
+}
+
+void tarefa_12(void)
+{
+    
+    static int a = 0;
+    for(;;){
+        for(volatile int i = 0; i < 1000000; i++);
+        TarefaEspera(1000);
+    }
+}
+
+/*
+ 2- Ambos os cÃ³digos tem o seguinte objetivo
+ * a primeira tarefa tem a funcao de piscar o led
+ * enquanto a segunda tem a funÃ§Ã£o de espera.
+ * 
+ * Ãs principais diferenÃ§as sÃ£o:
+ * 
+ * Como o preemptivo executa a funÃ§Ã£o em ordem de 
+ * prioridade, nÃ£o hÃ¡ funÃ§Ãµes como "TarefaSuspende(1)"
+ * ou "TarefaContinua(1);" que aparece nas tarefas 
+ * cooperativas, essas funÃ§Ãµes tem o objetivo de 
+ * definir uma ordem para as tarefas serem executadas.
+ */
